0990da2bd4386c11d4f0b533dd739d6243202217
Basik713 <74111496+Basik713@users.noreply.github.com>
2020-11-24 10:42:01 +0000

    Update pool.js

--- scripts/pool.js
+++ scripts/pool.js
@@ -1,1 +1,23 @@
-{\rtf1}
+class Application {
+
+  constructor(options={}) {
+
+    /**
+
+     * The options provided to this application upon initialization
+
+     * @type {Object}
+
+     */
+
+    this.options = mergeObject(this.constructor.defaultOptions, options, {
+
+      insertKeys: true,
+
+      insertValues: true,
+
+      overwrite: true,
+
+      inplace: false
+
+    });
