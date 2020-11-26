83dce1839f887e7657e75d6a87386f06532b613d
Basik713 <74111496+Basik713@users.noreply.github.com>
2020-11-26 20:44:19 +0000

    Update pool.js

--- scripts/pool.js
+++ scripts/pool.js
@@ -1,24 +1,18 @@
-const dataArray = ["Hello","Hi","Wassup"];
-
-export default class MessageWindow extends Application {
-    static get defaultOptions() {
-        return {
-            ...super.defaultOptions,
-            id : "messagewindow",
-            template : "modules/tension-pool/templates/tensionpool.html",
-            resizable : false,
-            height : "auto",
-            width : 400,
-            minimizable : true,
-            title : "LPTips Message",
-            renderContext: "Does this work?"
+Hooks.on('init' => () {
+    class MessageWindow extends Application {
+        static get defaultOptions() {
+            return {
+                ...super.defaultOptions,
+                id : "messagewindow",
+                template : "modules/tension-pool/templates/tensionpool.html",
+                resizable : false,
+                height : "auto",
+                width : 200,
+                minimizable : true,
+                title : "Tension Pool",
+                //renderContext: "Does this work?"
+            }
         }
     }
-
-  getData() {
-    return {
-      messages: this.object
-  }
-}
-}
-new MessageWindow(dataArray).render(true);
+    new MessageWindow().render(true);
+})
