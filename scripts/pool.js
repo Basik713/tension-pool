2c855a4c5b9d9992f62db1b2ceee41fd37b784e2
Basik713 <74111496+Basik713@users.noreply.github.com>
2020-11-27 12:45:09 +0000

    Update pool.js

--- scripts/pool.js
+++ scripts/pool.js
@@ -1,18 +1,14 @@
-Hooks.on('init' => () {
-    class MessageWindow extends Application {
-        static get defaultOptions() {
-            return {
-                ...super.defaultOptions,
-                id : "messagewindow",
-                template : "modules/tension-pool/templates/tensionpool.html",
-                resizable : false,
-                height : "auto",
-                width : 200,
-                minimizable : true,
-                title : "Tension Pool",
-                //renderContext: "Does this work?"
-            }
+class poolWindow extends Application {
+     static get defaultOptions() {
+        return {
+              ...super.defaultOptions,
+              id : "messagewindow",
+              template : "modules/tension-pool/templates/tensionpool.html",
+              resizable : false,
+              height : "auto",
+              width : 200,
+              minimizable : true,
+               title : "Tension Pool",
         }
-    }
-    new MessageWindow().render(true);
-})
+     }
+}
