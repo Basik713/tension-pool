5995ccaf986e77a162a498ddebba24823e159744
Basik713 <74111496+Basik713@users.noreply.github.com>
2020-11-30 16:29:37 +0000

    Update pyramid.js

--- scripts/pyramid.js
+++ scripts/pyramid.js
@@ -3,7 +3,7 @@ class pyramidWindow extends Application {
         return {
               ...super.defaultOptions,
               id : "pyramidwindow",
-              template : "modules/tension-pool/templates/tensionpool.html",
+              template : "./templates/tensionpool.html",
               resizable : false,
               height : "auto",
               width : 200,
